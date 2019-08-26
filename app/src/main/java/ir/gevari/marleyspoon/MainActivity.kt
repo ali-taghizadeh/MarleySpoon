67bd1c76b3278851b57bb5c2757c37c07ec3b46d
Ali Taghizadeh <ali.taghizadeh@outlook.com>
2019-08-26 21:53:02 +0430

    add dependencies and credentials

--- app/src/main/java/ir/gevari/marleyspoon/MainActivity.kt
+++ app/src/main/java/ir/gevari/marleyspoon/MainActivity.kt
@@ -1,7 +1,7 @@
 package ir.gevari.marleyspoon
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
