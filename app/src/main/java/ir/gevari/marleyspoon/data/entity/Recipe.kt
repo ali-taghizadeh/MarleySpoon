fb64e346bc67ffea3e94d0f642473b08cbe5b49e
Ali Taghizadeh <ali.taghizadeh@outlook.com>
2019-08-26 21:57:45 +0430

    add room entity for recipes

---
+++ app/src/main/java/ir/gevari/marleyspoon/data/entity/Recipe.kt
@@ -0,0 +1,24 @@
+package ir.gevari.marleyspoon.data.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "recipe_list")
+data class Recipe(
+    @PrimaryKey
+    val title: String,
+    val description: String,
+    val chef: Chef,
+    val tags: List<Tags>,
+    val photo: String
+) {
+    companion object
+}
+
+data class Tags(val name: String) {
+    companion object
+}
+
+data class Chef(val name: String) {
+    companion object
+}
