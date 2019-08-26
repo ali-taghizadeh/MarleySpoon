5f9411fba6cee51b55a26b98e5b5a4d9c461fddc
Ali Taghizadeh <ali.taghizadeh@outlook.com>
2019-08-27 00:44:56 +0430

    set fetched data in DetailsActivity layout

--- app/src/main/java/ir/gevari/marleyspoon/view/details/DetailsActivity.kt
+++ app/src/main/java/ir/gevari/marleyspoon/view/details/DetailsActivity.kt
@@ -1,11 +1,19 @@
 package ir.gevari.marleyspoon.view.details
 
 import android.os.Bundle
+import android.text.SpannableStringBuilder
+import androidx.core.content.ContextCompat
+import androidx.core.text.backgroundColor
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import ir.gevari.marleyspoon.R
 import ir.gevari.marleyspoon.view.base.ScopedActivity
-import kotlinx.android.synthetic.main.activity_recipe_list.*
+import kotlinx.android.synthetic.main.activity_details.*
+import kotlinx.android.synthetic.main.activity_recipe_list.collapsing
+import kotlinx.android.synthetic.main.activity_recipe_list.imageViewPhoto
+import kotlinx.android.synthetic.main.activity_recipe_list.toolbar
 import kotlinx.coroutines.launch
 import org.kodein.di.KodeinAware
 import org.kodein.di.android.closestKodein
@@ -31,6 +39,22 @@ class DetailsActivity : ScopedActivity(), KodeinAware {
         val details = viewModel.details.await()
         details.observe(this@DetailsActivity, Observer {
             if (it == null || it.isEmpty()) return@Observer
+            val position: Int = intent?.extras?.get(getString(R.string.item_position)) as Int
+            Glide
+                .with(this@DetailsActivity)
+                .load(it[position].photo)
+                .apply(RequestOptions.fitCenterTransform())
+                .into(imageViewPhoto)
+            collapsing.title = it[position].title
+            textViewChef.text = it[position].chef.name
+            textViewDescription.text = it[position].description
+            val tags = SpannableStringBuilder()
+            for (tag in it[position].tags)
+                tags.backgroundColor(ContextCompat.getColor(this@DetailsActivity, R.color.gray)) {
+                    append(" " + tag.name + " ")
+                }
+                    .append(" ")
+            textViewTags.text = tags
         })
     }
 }
